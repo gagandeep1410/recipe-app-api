90984896a59e1b0b7906bb634b26ba8da5e83936
Gagandeep Singh <gagandeepsingh1410@gmail.com>
2022-08-30 11:02:06 +0530

    correction

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,7 +5,7 @@ Django command to wait for DB availability.
 
 from django.core.management.base import BaseCommand
 
-class command(BaseCommand):
+class Command(BaseCommand):
 
     def handle(self, *args, **options):
         pass
