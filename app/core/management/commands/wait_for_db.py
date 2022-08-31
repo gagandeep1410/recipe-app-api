42bfd91704d577d0f851859409b4beca6c18299b
Gagandeep Singh <gagandeepsingh1410@gmail.com>
2022-08-31 09:13:22 +0530

    added DB migrate command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,9 +3,23 @@ Django command to wait for DB availability.
 
 """
 
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2Error, OperationalError):
+                self.stderr.write('Database unavailable.. waiting for 1 sec')
+                time.sleep(1)     
+        self.stdout.write(self.style.SUCCESS('Database Available!'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,7 @@
 """Test Django management commands"""
 
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -9,13 +9,13 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    def test_wait_for_db_ready(self,patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=["default"])
-        
+        patched_check.assert_called_once_with(databases=["default"])
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
